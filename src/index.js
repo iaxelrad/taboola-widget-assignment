350fc30bfefa8f99f571725ca7e80c4f4493f40a
Itamar Axelrad <iaxelrad@gmail.com>
2021-06-08 11:15:08 +0300

    Some stying fixes for the header.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import './style.css';
 import { renderRecommendationList } from './render-recommendation-list';
 
-const mainDiv = document.createElement('div');
-mainDiv.className = 'widget-container';
+(function () {
+  const mainDiv = document.createElement('div');
+  mainDiv.className = 'widget-container';
 
-const widgetHeader = document.createElement('div');
-widgetHeader.className = 'widget-header';
+  const widgetHeader = document.createElement('div');
+  widgetHeader.className = 'widget-header';
 
-const leftContentHeader = document.createElement('h1');
-leftContentHeader.innerHTML = 'MORE FOR YOU';
+  const leftContentHeader = document.createElement('h1');
+  leftContentHeader.className = 'left-content-header';
+  leftContentHeader.innerHTML = 'MORE FOR YOU';
 
-const widgetSpacer = document.createElement('div');
-widgetSpacer.className = 'widget-spacer';
+  const widgetSpacer = document.createElement('div');
+  widgetSpacer.className = 'widget-spacer';
 
-const rightContentHeader = document.createElement('p');
-rightContentHeader.innerHTML = 'Promoted Links by Taboola';
+  const rightContentHeader = document.createElement('p');
+  rightContentHeader.className = 'right-content-header';
+  rightContentHeader.innerHTML = 'Promoted Links by Taboola';
 
-widgetHeader.appendChild(leftContentHeader);
-widgetHeader.appendChild(widgetSpacer);
-widgetHeader.appendChild(rightContentHeader);
+  widgetHeader.appendChild(leftContentHeader);
+  widgetHeader.appendChild(widgetSpacer);
+  widgetHeader.appendChild(rightContentHeader);
 
-const listWrapper = document.createElement('div');
-listWrapper.className = 'list-wrapper';
+  const listWrapper = document.createElement('div');
+  listWrapper.className = 'list-wrapper';
 
-const list = document.createElement('ul');
-listWrapper.appendChild(list);
+  const list = document.createElement('ul');
+  listWrapper.appendChild(list);
 
-renderRecommendationList(list);
+  renderRecommendationList(list);
 
-mainDiv.appendChild(widgetHeader);
-mainDiv.appendChild(listWrapper);
-document.body.appendChild(mainDiv);
+  mainDiv.appendChild(widgetHeader);
+  mainDiv.appendChild(listWrapper);
+  document.body.appendChild(mainDiv);
+})();
