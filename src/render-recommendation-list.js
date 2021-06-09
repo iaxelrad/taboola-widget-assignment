48733c1bb93014727c668c9991c34eccbb668ab8
Itamar Axelrad <iaxelrad@gmail.com>
2021-06-09 23:18:26 +0300

    added try-catch for better error handling.

--- src/render-recommendation-list.js
+++ src/render-recommendation-list.js
@@ -7,36 +7,40 @@ import {
 } from './elements';
 
 export const renderRecommendationList = async list => {
-  const recList = await fetchRecommendations();
+  try {
+    const recList = await fetchRecommendations();
 
-  recList.map(({ url, thumbnail, description, branding, origin }) => {
-    const recItem = document.createElement('li');
-    const recItemWrapper = createAnchorTag(url, origin);
-    const recDescriptionWrapper = createDivTag('rec-description-wrapper fade');
-    const recDescription = createTextElement(
-      'h1',
-      'rec-description',
-      description
-    );
-    const recPublisher = createTextElement('p', 'rec-publisher', branding);
-    const recThumbnail = createVisualElement(
-      'img',
-      'rec-item-thumbnail',
-      thumbnail[0].url
-    );
-    const recVideoThumbnail = createVisualElement(
-      'iframe',
-      'rec-item-thumbnail',
-      thumbnail[0].url
-    );
+    recList.map(({ url, thumbnail, description, branding, origin }) => {
+      const recItem = document.createElement('li');
+      const recItemWrapper = createAnchorTag(url, origin);
+      const recDescriptionWrapper = createDivTag(
+        'rec-description-wrapper fade'
+      );
+      const recDescription = createTextElement(
+        'h1',
+        'rec-description',
+        description
+      );
+      const recPublisher = createTextElement('p', 'rec-publisher', branding);
+      const recThumbnail = createVisualElement(
+        'img',
+        'rec-item-thumbnail',
+        thumbnail[0].url
+      );
+      const recVideoThumbnail = createVisualElement(
+        'iframe',
+        'rec-item-thumbnail',
+        thumbnail[0].url
+      );
 
-    recDescriptionWrapper.appendChild(recDescription);
-    recItemWrapper.appendChild(
-      origin === 'video' ? recVideoThumbnail : recThumbnail
-    );
-    recItemWrapper.appendChild(recDescriptionWrapper);
-    recItemWrapper.appendChild(recPublisher);
-    recItem.appendChild(recItemWrapper);
-    list.appendChild(recItem);
-  });
+      recDescriptionWrapper.appendChild(recDescription);
+      recItemWrapper.appendChild(
+        origin === 'video' ? recVideoThumbnail : recThumbnail
+      );
+      recItemWrapper.appendChild(recDescriptionWrapper);
+      recItemWrapper.appendChild(recPublisher);
+      recItem.appendChild(recItemWrapper);
+      list.appendChild(recItem);
+    });
+  
 };
