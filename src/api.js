36edbbc883ae185255b086cd0eae985e2aaf1b41
Itamar Axelrad <iaxelrad@gmail.com>
2021-06-09 23:16:22 +0300

    Added layer of security to the API_KEY.

--- src/api.js
+++ src/api.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
-
+import { API_KEY } from '../config';
 export async function fetchRecommendations() {
   const baseUrl =
     'http://api.taboola.com/1.0/json/taboola-templates/recommendations.get';
-  const apiKey = 'f9040ab1b9c802857aa783c469d0e0ff7e7366e4';
   const appType = 'desktop';
   const sourceId = '214321562187';
   const sourceUrl = `http://www.site.com/videos/${sourceId}.html`;
   const url = `
-  ${baseUrl}?app.type=${appType}&app.apikey=${apiKey}&count=4&source.type=video&source.id=${sourceId}&source.url=${sourceUrl}`;
+  ${baseUrl}?app.type=${appType}&app.apikey=${API_KEY}&count=4&source.type=video&source.id=${sourceId}&source.url=${sourceUrl}`;
 
   try {
     const response = await axios.get(url);
