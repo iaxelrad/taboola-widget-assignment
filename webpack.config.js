8ee51b8c1c7a18f53f7a4f299c0347c4c17b6fc1
Itamar Axelrad <iaxelrad@gmail.com>
2021-06-09 12:45:05 +0300

    resctructured webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = env => {
   const isDevBuild = !(env && env.prod);
@@ -13,19 +12,18 @@ module.exports = env => {
       output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        // clean: true,
+        clean: true,
       },
       devServer: {
         contentBase: './dist',
+        hot: true,
       },
       plugins: [
         new HtmlWebpackPlugin({
           title: 'Taboola Widget',
         }),
-        new CopyPlugin({
-          patterns: [{ from: 'index.html', to: '/dist' }],
-        }),
       ],
+      devtool: 'inline-source-map',
       module: {
         rules: [
           { test: /\.html$/i, use: 'html-loader' },
